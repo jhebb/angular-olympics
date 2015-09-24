1bd0b29f62dd129741949d7451adf01756c533c9
Justin Hebb <justin@jukah.com>
2015-09-24 17:39:06 -0300

    add some basic sorting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,35 +5,27 @@
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope', '$http', function($scope, $http, $watchGroup) {
 
-    $scope.rankFilter = function (rank) {
-        return rank.rank > 0;
-    };
+    $scope.gold_value = 3;
+    $scope.silver_value = 2;
+    $scope.bronze_value = 1;
 
-   $scope.gold_value = 3;
-   $scope.silver_value = 2;
-   $scope.bronze_value = 1;
+    $scope.sortType     = 'total_points'; // set the default sort type
+    $scope.sortReverse  = true;  // set the default sort order
 
-   $scope.$watchGroup(['gold_value'],function(newVal, oldVal){
-    // console.log('Using $scope.$watchGroup - Models Changed to '+ newValues[0]+' and '+newValues[1]);
-    console.log('Using $scope.$watchGroup - Models Changed to '+ newVal[0]);
 
-   });
+    $http.get('https://www.kimonolabs.com/api/4rgltdoq?apikey=h8I2BEdJd3zQHmVYN8aboiHgoy71W4BX')
+      .then(function(res){
+        $scope.countries = res.data.results.data;
 
-  $http.get('https://www.kimonolabs.com/api/4rgltdoq?apikey=h8I2BEdJd3zQHmVYN8aboiHgoy71W4BX')
-    .then(function(res){
-      $scope.countries = res.data.results.data;
+        angular.forEach($scope.countries, function (country) {
+          country.gold = parseFloat(country.gold);
+          country.silver = parseFloat(country.silver);
+          country.bronze = parseFloat(country.bronze);
+        });
 
-      angular.forEach($scope.countries, function (country) {
-        country.gold = parseFloat(country.gold);
-        country.silver = parseFloat(country.silver);
-        country.bronze = parseFloat(country.bronze);
-        country.gold_points = country.gold * $scope.gold_value;
-        country.silver_points = country.silver * $scope.silver_value;
-        country.bronze_points = country.bronze * $scope.bronze_value;
-        country.total_points = country.gold_points + country.silver_points + country.bronze_points;
+        $scope.updatePoints();
+
       });
-
-    });
 
     $scope.updatePoints = function() {
       angular.forEach($scope.countries, function (country) {
@@ -52,28 +44,15 @@ angular.module('myApp.controllers', []).
       $scope.updatePoints();
     }
 
-    // $scope.goldPoints = function(index) {
-    //   var country = $scope.countries[index];
-
-    //   return country.gold * $scope.gold_value;
-    // }
-
-    // $scope.totalPoints = function(index) {
-    //   var country = $scope.countries[index];
-
-    //   // return $scope.goldPoints(index)
-    //   return 10 * $scope.gold_value;
-    // }
-
 
     // $scope.$watchGroup(['gold_value', 'silver_value', 'bronze_value'],function(newVal, oldVal){
     //   // $scope.$apply();
 
     //   angular.forEach($scope.countries, function (country) {
-    //     country.gold_points = country.gold * newVal[0];
+    //     country.gold_points = country.gold * $scope.gold_value;
     //     country.silver_points = country.silver * $scope.silver_value;
     //     country.bronze_points = country.bronze * $scope.bronze_value;
-    //     // country.total_points = 10 * newVal[0] + country.silver_points + country.bronze_points;
+    //     country.total_points = country.gold_points + country.silver_points + country.bronze_points;
     //   });
     // });
   }])
